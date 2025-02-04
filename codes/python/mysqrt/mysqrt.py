06b183e6d66f2edec9bffd5033e6b62b5d2763a8
moazhar64 <mirazharudin64@gmail.com>
2025-02-04 12:58:33 +0530

    Added a check for tolerance

--- codes/python/mysqrt/mysqrt.py
+++ codes/python/mysqrt/mysqrt.py
@@ -1,7 +1,13 @@
-x=2.0
+x=100000.0
 s=1.0
-kmax=10
+kmax=100
+tol=1.0e-14
 for k in range(kmax):
-	print(f"At iteration {k} the value of s={s}") 	
+	print(f"At iteration {k} the value of s={s}")
+	s0=s 	
 	s=0.5*(s + (x/s))
-print(f"Finally, at iteration {k} the value of s={s}")
+	delta_s=s-s0
+	print(f"delta_s={delta_s}")
+	if(abs(delta_s/x) < tol):
+		break
+print(f"Finally, the value of s={s}")
