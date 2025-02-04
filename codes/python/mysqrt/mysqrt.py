d7f8ad4bf2e25196f1be68c28435fab67ff1e5db
moazhar64 <mirazharudin64@gmail.com>
2025-02-04 14:32:08 +0530

    added a check for negative numbers

--- codes/python/mysqrt/mysqrt.py
+++ codes/python/mysqrt/mysqrt.py
@@ -6,6 +6,11 @@ def sqrt2(x,debug=False):
 	"""
 	sqrt function implemenaion
 	"""
+	from numpy import nan
+	if x==0:
+		return 0
+	elif x<0:
+		return nan
 	s=1.0
 	kmax=100
 	tol=1.0e-14
@@ -15,7 +20,6 @@ def sqrt2(x,debug=False):
 		s0=s 	
 		s=0.5*(s + (x/s))
 		delta_s=s-s0
-		print(f"delta_s={delta_s}")
 		if(abs(delta_s/x) < tol):
 			break
 	if debug:
