f95f3bd63a6b9df842e1b5a07ac4dca60017ee48
moazhar64 <mirazharudin64@gmail.com>
2025-02-04 14:14:35 +0530

    use of debug

--- codes/python/mysqrt/mysqrt.py
+++ codes/python/mysqrt/mysqrt.py
@@ -2,7 +2,7 @@
 Module to calculate sqrt using Newton's method
 """
 
-def sqrt2(x):
+def sqrt2(x,debug=False):
 	"""
 	sqrt function implemenaion
 	"""
@@ -10,11 +10,14 @@ def sqrt2(x):
 	kmax=100
 	tol=1.0e-14
 	for k in range(kmax):
-		print(f"At iteration {k} the value of s={s}")
+		if debug:
+			print(f"At iteration {k} the value of s={s}")
 		s0=s 	
 		s=0.5*(s + (x/s))
 		delta_s=s-s0
 		print(f"delta_s={delta_s}")
 		if(abs(delta_s/x) < tol):
 			break
-	print(f"Finally, the value of s={s}")
+	if debug:
+		print(f"Finally, the value of s={s}")
+	return s
